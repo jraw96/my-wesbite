ef4002138e0939b05a01f3d5ae986da43b2392fc
Jake Raw <jraw@apple.com>
2022-10-15 14:01:15 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li>
+        <a href="#">home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Travel</a>
+      </li>
+      <li>
+        <a href="#">Books</a>
+      </li>
+    </ul>
   );
 }
 
