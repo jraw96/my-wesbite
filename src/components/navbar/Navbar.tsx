85706bb0625b3bbefc3a08cc514a93ee88929095
Jake Raw <jraw@apple.com>
2022-10-15 16:36:27 -0500

    added debounce logic

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { debounce } from 'lodash';
+import React, { useCallback, useState } from 'react';
 
 import './Navbar.scss';
 
@@ -17,16 +18,30 @@ function Navbar() {
     setPage(page);
   };
 
+  const isHoveringOnNav = (page: any) => {
+    isHoveringOn(page);
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const hoveringCallback = useCallback(
+    debounce((page) => isHoveringOnNav(page), 300),
+    []
+  );
+
   return (
     <ul>
       <div className="hr-container">
-        <hr />
+        <hr
+          className={`hr-settings ${
+            activePage === pages.home ? 'show-hr animate' : 'hide-hr'
+          }`}
+        />
       </div>
       <li>
         <a
           onClick={() => clickedNavButton(pages.home)}
-          onMouseEnter={() => isHoveringOn(pages.home)}
-          onMouseLeave={() => isHoveringOn('')}
+          onMouseEnter={() => hoveringCallback(pages.home)}
+          onMouseLeave={() => hoveringCallback('')}
           className={`${activePage === pages.home ? 'current-page' : ''} ${
             activePage === pages.home && isHovered && isHovered !== pages.home
               ? 'half-opacity'
@@ -38,13 +53,19 @@ function Navbar() {
         </a>
       </li>
       <div className="hr-container">
-        <hr />
+        <hr
+          className={`hr-settings ${
+            activePage === pages.about || activePage === pages.home
+              ? 'show-hr animate'
+              : 'hide-hr'
+          }`}
+        />
       </div>
       <li>
         <a
           onClick={() => clickedNavButton(pages.about)}
-          onMouseEnter={() => isHoveringOn(pages.about)}
-          onMouseLeave={() => isHoveringOn('')}
+          onMouseEnter={() => hoveringCallback(pages.about)}
+          onMouseLeave={() => hoveringCallback('')}
           className={`${activePage === pages.about ? 'current-page' : ''} ${
             activePage === pages.about && isHovered && isHovered !== pages.about
               ? 'half-opacity'
@@ -56,13 +77,19 @@ function Navbar() {
         </a>
       </li>
       <div className="hr-container">
-        <hr />
+        <hr
+          className={`hr-settings ${
+            activePage === pages.about || activePage === pages.travel
+              ? 'show-hr animate'
+              : 'hide-hr'
+          }`}
+        />
       </div>
       <li>
         <a
           onClick={() => clickedNavButton(pages.travel)}
-          onMouseEnter={() => isHoveringOn(pages.travel)}
-          onMouseLeave={() => isHoveringOn('')}
+          onMouseEnter={() => hoveringCallback(pages.travel)}
+          onMouseLeave={() => hoveringCallback('')}
           className={`${activePage === pages.travel ? 'current-page' : ''} ${
             activePage === pages.travel &&
             isHovered &&
@@ -76,13 +103,19 @@ function Navbar() {
         </a>
       </li>
       <div className="hr-container">
-        <hr />
+        <hr
+          className={`hr-settings ${
+            activePage === pages.travel || activePage === pages.books
+              ? 'show-hr animate'
+              : 'hide-hr'
+          }`}
+        />
       </div>
       <li>
         <a
           onClick={() => clickedNavButton(pages.books)}
-          onMouseEnter={() => isHoveringOn(pages.books)}
-          onMouseLeave={() => isHoveringOn('')}
+          onMouseEnter={() => hoveringCallback(pages.books)}
+          onMouseLeave={() => hoveringCallback('')}
           className={`${activePage === pages.books ? 'current-page' : ''} ${
             activePage === pages.books && isHovered && isHovered !== pages.books
               ? 'half-opacity'
@@ -94,7 +127,11 @@ function Navbar() {
         </a>
       </li>
       <div className="hr-container">
-        <hr />
+        <hr
+          className={`hr-settings ${
+            activePage === pages.books ? 'show-hr animate' : 'hide-hr'
+          }`}
+        />
       </div>
     </ul>
   );
