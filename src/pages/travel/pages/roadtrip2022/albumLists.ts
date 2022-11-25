a720c2dbd9b1dbbf8a9a74b0605c411df63e9d54
Jake Raw <jraw@apple.com>
2022-11-25 17:33:32 -0600

    Added road trip pics

--- src/pages/travel/pages/roadtrip2022/albumLists.ts
+++ src/pages/travel/pages/roadtrip2022/albumLists.ts
@@ -1,3 +1,4 @@
+import { TravelRoadTrip2022Imagees } from '../../../../util/images-urls';
 import { photoItem } from '../../../../util/types';
 
 export interface roadTrip2022Places {
@@ -10,13 +11,349 @@ export interface roadTrip2022Places {
   yosemite: photoItem[];
 }
 
-const grandCanyon: photoItem[] = [];
-const monumentValley: photoItem[] = [];
-const canyonLands: photoItem[] = [];
-const arches: photoItem[] = [];
-const deadHorse: photoItem[] = [];
-const zion: photoItem[] = [];
-const yosemite: photoItem[] = [];
+const grandCanyon: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip4,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 4',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip5,
+    width: 853,
+    height: 1138,
+    alt: 'Grand Canyon 5',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip2,
+    width: 6022,
+    height: 4024,
+    alt: 'Grand Canyon 2',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip3,
+    width: 6022,
+    height: 4024,
+    alt: 'Grand Canyon 3',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip6,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 6',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip7,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 7',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip8,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 8',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip9,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 9',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip10,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 10',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip11,
+    width: 853,
+    height: 1135,
+    alt: 'Grand Canyon 11',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip12,
+    width: 4032,
+    height: 3024,
+    alt: 'Grand Canyon 12',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.roadTrip13,
+    width: 853,
+    height: 1138,
+    alt: 'Grand Canyon 13',
+    id: 'thumbnail',
+  },
+];
+const monumentValley: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.monument3,
+    width: 727,
+    height: 969,
+    alt: 'Monument Valley 3',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.monument1,
+    width: 4032,
+    height: 3024,
+    alt: 'Monument Valley 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.monument2,
+    width: 12412,
+    height: 3866,
+    alt: 'Monument Valley 2',
+    id: 'thumbnail',
+  },
+];
+const canyonLands: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands1,
+    width: 1292,
+    height: 969,
+    alt: 'Canonylands 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands4,
+    width: 727,
+    height: 969,
+    alt: 'Canonylands 3',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands5,
+    width: 7850,
+    height: 3384,
+    alt: 'Canonylands 5',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands6,
+    width: 1292,
+    height: 969,
+    alt: 'Canonylands 6',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands8,
+    width: 1292,
+    height: 969,
+    alt: 'Canonylands 8',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands9,
+    width: 1292,
+    height: 969,
+    alt: 'Canonylands 9',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands10,
+    width: 1292,
+    height: 969,
+    alt: 'Canonylands 10',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.canyonlands11,
+    width: 727,
+    height: 969,
+    alt: 'Canonylands 11',
+    id: 'thumbnail',
+  },
+];
+const arches: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.arches1,
+    width: 727,
+    height: 969,
+    alt: 'Arches 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.arches2,
+    width: 9546,
+    height: 3950,
+    alt: 'Arches 2',
+    id: 'thumbnail',
+  },
+];
+const deadHorse: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.deadhorse1,
+    width: 1292,
+    height: 969,
+    alt: 'Dead Horse 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.deadhorse4,
+    width: 727,
+    height: 969,
+    alt: 'Dead Horse 4',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.deadhorse3,
+    width: 9316,
+    height: 3960,
+    alt: 'Dead Horse 3',
+    id: 'thumbnail',
+  },
+];
+const zion: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.zion1,
+    width: 1292,
+    height: 969,
+    alt: 'Zion 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion7,
+    width: 727,
+    height: 969,
+    alt: 'Zion 7',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion5,
+    width: 6022,
+    height: 4024,
+    alt: 'Zion 5',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion9,
+    width: 727,
+    height: 969,
+    alt: 'Zion 9',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion8,
+    width: 727,
+    height: 969,
+    alt: 'Zion 8',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion6,
+    width: 6022,
+    height: 4024,
+    alt: 'Zion 6',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion10,
+    width: 727,
+    height: 969,
+    alt: 'Zion 10',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion11,
+    width: 727,
+    height: 969,
+    alt: 'Zion 11',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion2,
+    width: 727,
+    height: 969,
+    alt: 'Zion 2',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion4,
+    width: 727,
+    height: 969,
+    alt: 'Zion 4',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.zion3,
+    width: 727,
+    height: 969,
+    alt: 'Zion 3',
+    id: 'thumbnail',
+  },
+];
+const yosemite: photoItem[] = [
+  {
+    src: TravelRoadTrip2022Imagees.yosemite1,
+    width: 1292,
+    height: 969,
+    alt: 'Yosemite 1',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite2,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 2',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite3,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 3',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite7,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 7',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite5,
+    width: 6022,
+    height: 4024,
+    alt: 'Yosemite 5',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite8,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 8',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite9,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 9',
+    id: 'thumbnail',
+  },
+  {
+    src: TravelRoadTrip2022Imagees.yosemite10,
+    width: 727,
+    height: 969,
+    alt: 'Yosemite 10',
+    id: 'thumbnail',
+  },
+];
 
 export const roadTrip2022List: roadTrip2022Places = {
   grandCanyon: grandCanyon,
